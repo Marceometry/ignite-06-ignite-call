4d213bde1935264f1a58c2fa3bd4cb72ce3a7374
Marcelino Teixeira <marcelino.teixeira.dev@gmail.com>
2023-08-24 17:14:11 -0300

    feat: add ignite-ui

---
+++ src/styles/global.ts
@@ -0,0 +1,16 @@
+import { globalCss } from '@ignite-ui/react'
+
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    padding: 0,
+    margin: 0,
+  },
+
+  body: {
+    color: '$gray100',
+    backgroundColor: '$gray900',
+    fontFamily: '$default',
+    '-webkit-font-smoothing': 'antialiased',
+  }
+})
